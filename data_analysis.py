d1daa587754c69b521eb3538086ddec3d34bfcd3
Aleksey <rusal@bk.ru>
2022-09-24 01:33:14 +0300

    add pickle

---
+++ data_analysis.py
@@ -0,0 +1,38 @@
+import pickle
+
+from find_hot_tables import find_hot_tables
+
+
+def read_file_to_df(file_name="log_week_end.csv"):
+    import pandas as pd
+    df = pd.read_csv(file_name)
+    df = df.sort_values(by='rn', ascending=True)
+    return df
+
+def save_to_pickle():
+    df = read_file_to_df("log_week_end.csv")
+    # print(df.columns)
+    # print(df)
+    with open('df.pickle', 'wb') as handle:
+        pickle.dump(df, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    data = df.values.tolist()
+    with open('data.pickle', 'wb') as handle:
+        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    hot_tables = find_hot_tables()
+    with open('hot_tables.pickle', 'wb') as handle:
+        pickle.dump(hot_tables, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def read_pickle():
+    with open('df.pickle', 'rb') as handle:
+        df = pickle.load(handle)
+    with open('data.pickle', 'rb') as handle:
+        data = pickle.load(handle)
+    with open('hot_tables.pickle', 'rb') as handle:
+        hot_tables = pickle.load(handle)
+
+    return (df, data, hot_tables)
+
+if __name__ == '__main__':
+    save_to_pickle()
+    (df, data, hot_tables) = read_pickle()
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from find_hot_tables import find_hot_tables
+
 
 if __name__ == '__main__':
+    from find_hot_tables import find_hot_tables
     hot_tables = find_hot_tables()
     print(hot_tables)
     print(f"\n\n\n{hot_tables[0][0]} {hot_tables[0][1]}")
